78406012dc5b6d3868a51b02d121cc2e381b3374
408-\student <o>
2024-10-28 16:22:49 +0900

    武器効果発動処理作成途中

--- Assets/Script/Player/WeaponUse/UseWeapon.cs
+++ Assets/Script/Player/WeaponUse/UseWeapon.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static WeaponManager;
 
 public class UseWeapon : MonoBehaviour
 {
-    private GameObject weapon;
+    [SerializeField]
+    [Header("weaponオブジェクト")] private GameObject weapon;
     private WeaponActivate active;
+
+    private void Start()
+    {
+        active = weapon.GetComponent<WeaponActivate>();
+    }
+
+    private void Update()
+    {
+        float fire = Input.GetAxis("Fire1");
+        if(fire > 0 )
+        {
+            active.Active();
+        }
+    }
 }
--- Assets/Script/Weapon/Kogetu/KogetuActiveProcess.cs
+++ Assets/Script/Weapon/Kogetu/KogetuActiveProcess.cs
@@ -7,6 +7,9 @@ public class KogetuActiveProcess : MonoBehaviour
     //アクセス用変数
     public static KogetuActiveProcess instance;
 
+    [SerializeField]
+    [Header("発動時間")] private float ac_time;
+
     private enum State
     {
         None,
@@ -15,9 +18,10 @@ public class KogetuActiveProcess : MonoBehaviour
         end
     }
 
-    private Vector3 firstPos;        //初期位置
-    private State nowstate = State.None;
-    private bool active_flg = false; //発動フラグ
+    private Vector3 firstPos;            //初期位置
+    private State nowstate = State.None; //
+    private bool active_flg = false;     //発動フラグ
+    private float ac_count = 0;          //発動カウント
 
     private void Start()
     {
@@ -45,6 +49,37 @@ public class KogetuActiveProcess : MonoBehaviour
 
         switch (nowstate)
         {
+            case State.start: //効果発動
+                //移動
+                transform.position = firstPos;
+                transform.position = new Vector3(firstPos.x, firstPos.y, firstPos.z + 0.5f);
+                nowstate = State.motion; //状態移行
+                break;
+
+            case State.motion: //発動中
+                Debug.Log("t"); //ここ来てない
+                //時間カウント
+                ac_count += Time.deltaTime;
+                //効果終了判定
+                if(ac_count > ac_time)
+                {
+                    nowstate = State.end; //状態移行
+                }
+                else
+                {
+                    //オブジェクトx軸回転
+                    Quaternion rot = Quaternion.AngleAxis(10.0f, new Vector3(1.0f, 0.0f, 0.0f));
+                    transform.rotation = rot;
+                }
+                break;
+
+            case State.end: //終了
+                //初期位置に移動
+                transform.position = firstPos;
+                //カウント初期化
+                ac_count = 0;
+                nowstate = State.None;
+                break;
         }
     }
 }
